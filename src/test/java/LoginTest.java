6fa6760006623c576ce000737a831f0a6ff823c2
blzoli <euxhhx@inf.elte.hu>
2026-05-12 16:03:23 +0200

    Project start

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,16 @@
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
+import java.net.MalformedURLException;
+import org.openqa.selenium.support.ui.*;
+
+
+
+import org.junit.*;
+
+public class LoginTest {
+
+
+};
