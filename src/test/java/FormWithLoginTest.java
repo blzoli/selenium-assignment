552ce726847a9f8eca9ac6f57be7d4169f439799
blzoli <euxhhx@inf.elte.hu>
2026-05-15 20:02:08 +0200

    Serach testing and form in logged in mail

--- src/test/java/FormWithLoginTest.java
+++ src/test/java/FormWithLoginTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 
 public class FormWithLoginTest extends BaseCsfdTest {
     @Test
-	public void canAccessMailBox() {
+	public void canSearchMailBox() {
 
         login("seleniumTest", "Selenium2026");
         assertLoggedIn();
@@ -13,6 +13,16 @@ public class FormWithLoginTest extends BaseCsfdTest {
 		this.driver.get("https://www.csfd.cz/posta/");
         // find text Pošta - Všechny zprávy 
         waitVisibility(By.xpath("//h1[contains(text(), 'Pošta - Všechny zprávy')]"));
+
+        WebElement searchInput = waitVisibility(By.id("frm-fulltextSearchForm-q")).click();
+
+        searchInput.sendKeys("csfd");
+        searchInput.sendKeys(Keys.ENTER);
+
+        // look for match <span class="match">ČSFD</span>
+
+        waitVisibility(By.xpath("//span[contains(@class, 'match') and contains(text(), 'ČSFD')]"));
+
 	}
 
 }
